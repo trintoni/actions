f15e59261bdea245d0f881fb0a6519d2a9e02fc7
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-17 13:57:18 -0300

    add environment

---
+++ teste/runshell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "#############################################"
+echo "Informacoes do host..."
+echo "${HOSTNAME}"
+echo "${USERNAME}"
+echo "#############################################"
