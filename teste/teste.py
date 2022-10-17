2de5b6ab365301aaa5d6c49aec33479d18cae198
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-17 11:02:14 -0300

    add functions

--- teste/teste.py
+++ teste/teste.py
@@ -1,4 +1,14 @@
-bicycles = ['trek', 'redline', 'specialized', 'cannondale'] # Criando a lista com conteúdo
+pods = ['b2b_database', 'b2b_api', 'b2b_backend', 'b2b_frontend'] # Criando a lista com conteúdo
 
-print("Imprimindo a lista bycicles: ")
-print(bicycles) # Imprimindo a lista bicycles
+print("Imprimindo a lista de PODs: ")
+print(pods) # Imprimindo a lista bicycles
+
+
+container_cpu = [20, 50, 40]
+
+
+cpu_increase = lambda container_cpu: container_cpu * 0.1 
+
+cpu_increment = list(map(cpu_increase, container_cpu))
+
+print("Adicionando CPU: " + str(cpu_increment))
