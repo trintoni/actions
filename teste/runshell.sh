34fa0e26232ca9ecf36838f8d87764709509816a
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-24 14:23:22 -0300

    Deploy action

--- teste/runshell.sh
+++ teste/runshell.sh
@@ -3,7 +3,7 @@ echo "#############################################"
 echo "Informacoes do host (RUNNER)..."
 echo "${HOSTNAME}"
 echo "${USER}"
-echo "${AKS_SECRETS}"
+echo "PASSWORD É: ${AKS_SECRETS}"
 echo "#############################################"
 
 az login -p $AKS_SECRETS -u lulumv@hotmail.com
