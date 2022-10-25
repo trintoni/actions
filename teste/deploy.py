fad926ce86984c549ba38923e7f368aa285b8e0e
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-24 22:29:29 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -3,20 +3,14 @@ from azure.identity import DefaultAzureCredential
 import os
 # Classe subprocess para execução de comandos no SO
 import subprocess
-subprocess.run(["pwd"])
-subprocess.run(["ls", "-l"])
 
 idaks = os.environ.get('AZURE_CLIENT_ID')
 print("O SECRET É: " + str(idaks))
-
-
-
 
 # The default credential first checks environment variables for configuration as described above.
 # If environment configuration is incomplete, it will try managed identity.
 credential = DefaultAzureCredential()
 
-
 print(credential)
 
-subprocess.run(["kubectl", "apply", "-f", "deployment.yaml"])
+subprocess.run(["kubectl", "get", "pods"])
