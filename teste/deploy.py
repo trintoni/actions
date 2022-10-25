3888b5cbd3124e5e931d84b603aa2aa3d323ae9f
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 10:12:45 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -19,3 +19,50 @@ for resource_group in client.resource_groups.list():
     print(f"Resource group: {resource_group.name}")
 
 print(f"Successful credential: {credential._successful_credential.__class__.__name__}")
+
+
+
+
+from kubernetes import config , client
+from kubernetes.client import Configuration
+from kubernetes.client.api import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+import re 
+
+pod_namespace = "default"
+pod_regex = "treina"
+try:
+    config.load_kube_config()
+    c = Configuration().get_default_copy()
+except AttributeError:
+    c = Configuration()
+    c.assert_hostname = False
+Configuration.set_default(c)
+core_v1 = core_v1_api.CoreV1Api()
+
+def get_pod_name(core_v1,pod_namespace,pod_regex):    
+    ret = core_v1.list_namespaced_pod(pod_namespace)
+    for i in ret.items:
+        #print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        pod_name=i.metadata.name
+        if re.match(pod_regex, pod_name):
+            return pod_name
+
+def check_pod_existance(api_instance,pod_namespace,pod_name):
+    resp = None
+    try:
+        resp = api_instance.read_namespaced_pod(name=pod_name,namespace=pod_namespace)
+    except ApiException as e:
+        if e.status != 404:
+            print("Unknown error: %s" % e)
+            exit(1)
+    if not resp:
+        print("Pod %s does not exist. Create it..." % pod_name)
+
+
+
+pod_name=get_pod_name(core_v1,pod_namespace,pod_regex)
+check_pod_existance(core_v1,pod_namespace,pod_name)
+
+print("O nome do POD: " + pod_name)
