1e5cf3db27ac3ef53f82d21174c1a1dc6e7f8d8f
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-24 22:01:35 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -1,5 +1,4 @@
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient
 # Importando uma Classe (Para tratativas com o sistema operacional)
 import os
 # Classe subprocess para execução de comandos no SO
