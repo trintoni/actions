e770f5205a422e3f24e4a91564e8c115e8559a01
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 11:23:36 -0300

    Deploy action

--- teste/run.sh
+++ teste/run.sh
@@ -5,5 +5,6 @@ pip install kubernetes
 python --version
 python ${GITHUB_ACTION_PATH}/deploy.py
 az account set --subscription 1163f6b0-6d5e-486b-9299-9a346dd84823
+az aks get-credentials --resource-group trinta_aks --name aks_treinamento
 az login --service-principal -u ceb340c7-ab28-43eb-b5ef-7cf0f90f6190 --password QrP8Q~8RcisVQziJZHlr3qdeG9tKqzHTRdch2djg --tenant bf8ff7cc-d91d-470e-aaee-42bb39736a2a
 kubectl get pods
