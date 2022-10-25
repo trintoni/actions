24d1e61d085cabb716660ff345166b062e95b804
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 11:20:08 -0300

    Deploy action

--- teste/run.sh
+++ teste/run.sh
@@ -4,5 +4,6 @@ pip install kubernetes
 # python ${GITHUB_ACTION_PATH}/teste.py
 python --version
 python ${GITHUB_ACTION_PATH}/deploy.py
-az login --service-principal -u "ceb340c7-ab28-43eb-b5ef-7cf0f90f6190" --password QrP8Q~8RcisVQziJZHlr3qdeG9tKqzHTRdch2djg --tenant bf8ff7cc-d91d-470e-aaee-42bb39736a2a
+az account set --subscription 1163f6b0-6d5e-486b-9299-9a346dd84823
+az login --service-principal -u ceb340c7-ab28-43eb-b5ef-7cf0f90f6190 --password QrP8Q~8RcisVQziJZHlr3qdeG9tKqzHTRdch2djg --tenant bf8ff7cc-d91d-470e-aaee-42bb39736a2a
 kubectl get pods
