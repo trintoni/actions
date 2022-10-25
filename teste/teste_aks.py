f82c9926bdf61b380a535c2027cdc36911a0b56d
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 10:39:24 -0300

    Deploy action

--- teste/teste_aks.py
+++ teste/teste_aks.py
@@ -59,3 +59,14 @@ pod_name=get_pod_name(core_v1,pod_namespace,pod_regex)
 check_pod_existance(core_v1,pod_namespace,pod_name)
 
 print("O nome do POD: " + pod_name)
+
+
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
