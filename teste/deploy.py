bd80b05b901342df212fa67f9ec1e94e785243f4
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-24 22:00:18 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -1,3 +1,5 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 # Importando uma Classe (Para tratativas com o sistema operacional)
 import os
 # Classe subprocess para execução de comandos no SO
@@ -5,7 +7,15 @@ import subprocess
 subprocess.run(["pwd"])
 subprocess.run(["ls", "-l"])
 
-
 idaks = os.environ.get('AZURE_CLIENT_ID')
-
 print("O SECRET É: " + idaks)
+
+
+
+
+# The default credential first checks environment variables for configuration as described above.
+# If environment configuration is incomplete, it will try managed identity.
+credential = DefaultAzureCredential()
+
+
+print(credential)
