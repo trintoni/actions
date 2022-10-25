cbbe7f75743123282fa0f994b81f1d70167b1b60
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 10:17:18 -0300

    Deploy action

--- teste/run.sh
+++ teste/run.sh
@@ -1,5 +1,6 @@
 pip install azure-identity
 pip install azure-mgmt-resource
+pip install kubernetes
 # python ${GITHUB_ACTION_PATH}/teste.py
 python --version
 python ${GITHUB_ACTION_PATH}/deploy.py
