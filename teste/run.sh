f8116fe88099b8546cfbe0983a75e13784ad66f6
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 11:07:24 -0300

    Deploy action

--- teste/run.sh
+++ teste/run.sh
@@ -4,4 +4,4 @@ pip install kubernetes
 # python ${GITHUB_ACTION_PATH}/teste.py
 python --version
 python ${GITHUB_ACTION_PATH}/deploy.py
-
+az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
