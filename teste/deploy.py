2d54766f3683d4d0ae0d185bd8227500048d64f8
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-25 09:25:40 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -4,12 +4,11 @@ import os
 # Classe subprocess para execução de comandos no SO
 import subprocess
 
-idaks = os.environ.get('AZURE_CLIENT_ID')
-print("O SECRET É: " + str(idaks))
-
 # The default credential first checks environment variables for configuration as described above.
 # If environment configuration is incomplete, it will try managed identity.
 credential = DefaultAzureCredential()
+
+
 
 print(credential)
 
