53a6947257682d137eab2594da2bf041e4ea5f76
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-26 15:50:11 -0300

    Deploy action

--- teste/deploy.py
+++ teste/deploy.py
@@ -5,11 +5,21 @@ import os
 # Classe subprocess para execução de comandos no SO
 import subprocess
 
+from kubernetes import config, client
+from kubernetes.client import Configuration
+from kubernetes.client.api import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+import kubernetes.client
+import re 
+from os import path
+import yaml
+
 
 print("AZURE_CLIENT_ID: " + os.environ.get('AZURE_CLIENT_ID'))
 print("AZURE_TENANT_IDL: " + os.environ.get('AZURE_TENANT_ID'))
 print("AZURE_CLIENT_SECRET: " + os.environ.get('AZURE_CLIENT_SECRET'))
-print("FILE_DEPLOYMENT: " + os.environ.get('FILE_DEPLOYMENT'))
+FILE_DEPLOYMENT = os.environ.get('FILE_DEPLOYMENT')
 
 # The default credential first checks environment variables for configuration as described above.
 # If environment configuration is incomplete, it will try managed identity.
@@ -36,3 +46,20 @@ print("Listing pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+
+
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+with open(path.join(path.dirname(__file__), FILE_DEPLOYMENT)) as f:
+    dep = yaml.safe_load(f)
+    k8s_apps_v1 = client.AppsV1Api()
+    resp = k8s_apps_v1.create_namespaced_deployment(body=dep, namespace="default")
+    print("Deployment created. status='%s'" % resp.metadata.name)
