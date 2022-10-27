4a4d16ec51023d6f2acf845eb88bcaa89ff4fc97
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-10-26 21:13:44 -0300

    Deploy action

--- teste/run.sh
+++ teste/run.sh
@@ -6,5 +6,6 @@ python --version
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
 az account set --subscription 1163f6b0-6d5e-486b-9299-9a346dd84823
 az aks get-credentials --resource-group trinta_aks --name aks_treinamento
+docker login trintaacr.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
 kubectl get pods
 python ${GITHUB_ACTION_PATH}/deploy.py
