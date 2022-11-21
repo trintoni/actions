56fc7b01a90f7668143ada1647c43134cba9b50a
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-11-21 19:31:25 -0300

    Fix actions

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -5,8 +5,9 @@ client = docker.from_env()
 
 PODNAME = os.environ.get('POD_NAME')
 WORKSPACE = os.environ.get('WORKSPACE')
+GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 
-build = client.images.build(path = WORKSPACE, tag = 'core.harbor.domain/library/' + PODNAME)
+build = client.images.build(path = GITHUBWORKSPACE, tag = 'core.harbor.domain/library/' + PODNAME)
 
 client.login(registry='core.harbor.domain', username='admin', password='Harbor12345')
 push = client.images.push('core.harbor.domain/library/' + PODNAME)
