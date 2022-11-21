ac48a7add4988889ecec4e49157e3d23e7f1a29b
Anderson Trintoni <anderson.trintoni@via.com.br>
2022-11-21 18:53:06 -0300

    Deploy

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -1,9 +1,15 @@
 import docker
+import os
+
 client = docker.from_env()
 
-build = client.images.build(path = "./")
+PODNAME = os.environ.get('POD_NAME')
+WORKSPACE = os.environ.get('WORKSPACE')
 
+build = client.images.build(path = WORKSPACE, tag = 'core.harbor.domain/library/' + PODNAME)
 
-
+client.login(registry='core.harbor.domain', username='admin', password='Harbor12345')
+push = client.images.push('core.harbor.domain/library/' + PODNAME)
+print(push)
 
 
